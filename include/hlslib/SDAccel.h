a1387389760ea73ef800e87bed61dce34fdb7309
Johannes de Fine Licht <johannes@musicmedia.dk>
2017-06-02 19:05:45 +0200

    Remove apple header, as we are expecting the Xilinx header

--- include/hlslib/SDAccel.h
+++ include/hlslib/SDAccel.h
@@ -14,11 +14,7 @@
 #include <string>
 #include <vector>
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#ifdef __APPLE__
-#include <OpenCL/opencl.h>
-#else
 #include <CL/opencl.h>
-#endif
 
 namespace hlslib {
 
