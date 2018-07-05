f0ce3d8097da4a34ff3bfd8ba385c112a082f48c
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2018-07-05 13:38:20 +0200

    Const flatten with automatic bit width computation, and added tests

--- include/hlslib/Flatten.h
+++ include/hlslib/Flatten.h
@@ -7,9 +7,13 @@
 #ifndef HLSLIB_SYNTHESIS
 #include <stdexcept>
 #endif
-#include <iostream>
+#include <cstddef>
+#include <ap_int.h>
+#include "hlslib/Utility.h"
 
 namespace hlslib {
+
+namespace {
 
 template <int i, typename T>
 struct Increment {
@@ -48,7 +52,8 @@ template <int i, typename T>
 struct Done {
   static bool Apply(T const *index, T const *ranges) {
     #pragma HLS INLINE
-    return index[i] == ranges[i * 3] && Done<i - 1, T>::Apply(index, ranges);
+    return (index[i] >= ranges[i * 3 + 1] - ranges[i * 3 + 2]) &&
+           Done<i - 1, T>::Apply(index, ranges);
   }
 };
 
@@ -57,13 +62,13 @@ struct Done<-1, T> {
   static bool Apply(T const*, T const*) { return true; }
 };
 
-template <class... ArgRanges>
-class FlattenImpl {
+template <class... Ranges>
+struct FlattenImpl {
 
-  static constexpr size_t kDims = sizeof...(ArgRanges) / 3;
+  static constexpr int kDims = sizeof...(Ranges) / 3;
 
  public:
-  FlattenImpl(ArgRanges &&... ranges) : ranges_{ranges...} {
+  FlattenImpl(Ranges &&... ranges) : ranges_{ranges...} {
     #pragma HLS INLINE
     for (int i = 0; i < kDims; ++i) {
       #pragma HLS UNROLL
@@ -86,7 +91,7 @@ class FlattenImpl {
     ++(*this);
   }
 
-  size_t size() const {
+  int size() const {
     #pragma HLS INLINE
     return Size<kDims - 1, int>::Apply(ranges_);
   }
@@ -101,9 +106,159 @@ private:
   const int ranges_[3 * kDims];
 };
 
-template <class... ArgRanges>
-FlattenImpl<ArgRanges...> Flatten(ArgRanges &&... ranges) {
-  return FlattenImpl<ArgRanges...>(std::forward<ArgRanges>(ranges)...);
+} // End anonymous namespace
+
+template <class... Ranges>
+FlattenImpl<Ranges...> Flatten(Ranges &&... ranges) {
+  return FlattenImpl<Ranges...>(std::forward<Ranges>(ranges)...);
 }
+
+namespace {
+
+template <signed long begin, signed long end>
+struct BitsToRepresent {
+  static constexpr unsigned kBits =
+      (begin < 0 || end < 0)
+          ? (ConstLog2(hlslib::max(hlslib::abs(begin), hlslib::abs(end))) + 1)
+          : ConstLog2(hlslib::max(begin, end));
+  using type = typename std::conditional<(begin < 0 || end < 0), ap_int<kBits>,
+                                         ap_uint<kBits>>::type;
+};
+
+template <int... ranges>
+struct ConstFlattenImpl;
+
+template <int dimIndex, int rangeBegin, int rangeEnd, int rangeStep>
+struct ConstFlattenImpl<dimIndex, rangeBegin, rangeEnd, rangeStep> {
+
+public:
+
+  static constexpr int kSize =
+      (rangeEnd - rangeBegin + rangeStep - 1) / rangeStep;
+
+  int operator[](int dim) {
+    #pragma HLS INLINE
+#ifndef HLSLIB_SYNTHESIS 
+    if (dim != dimIndex) {
+      throw std::runtime_error("Index out of bounds");
+    }
+#endif
+    return i_;
+  }
+
+  template <int>
+  int get() {
+    #pragma HLS INLINE
+    return i_;
+  }
+
+  bool Increment() {
+    #pragma HLS INLINE
+    if (i_ >= rangeEnd - rangeStep) {
+      i_ = rangeBegin;
+      return true;
+    } else {
+      i_ += rangeStep;
+    }
+    return false;  // Whether we wrapped over the end
+  }
+
+  bool last() const {
+    #pragma HLS INLINE
+    return i_ >= rangeEnd - rangeStep;
+  }
+
+private:
+  typename BitsToRepresent<rangeBegin, rangeEnd>::type i_{rangeBegin};
+};
+
+template <int dimIndex, int rangeBegin, int rangeEnd, int rangeStep,
+          int... ranges>
+struct ConstFlattenImpl<dimIndex, rangeBegin, rangeEnd, rangeStep,
+                       ranges...> {
+
+  static constexpr int kSize =
+      ConstFlattenImpl<dimIndex, ranges...>::kSize *
+      ConstFlattenImpl<dimIndex + 1, rangeBegin, rangeEnd, rangeStep>::kSize;
+
+  int operator[](int i) {
+    #pragma HLS INLINE
+    if (i == dimIndex) {
+      return i_;
+    } else {
+      return next_[i];
+    }
+  }
+
+  template <int dim>
+  int get() {
+    #pragma HLS INLINE
+    if (dim == dimIndex) {
+      return i_;
+    } else {
+      return next_.template get<dim>();
+    }
+  }
+
+  bool Increment() {
+    #pragma HLS INLINE
+    if (next_.Increment()) {
+      if (i_ >= rangeEnd - rangeStep) {
+        i_ = rangeBegin;
+        return true;
+      } else {
+        i_ += rangeStep;
+      }
+    }
+    return false; // Whether we wrapped over the end
+  }
+
+  bool last() const {
+    return (i_ >= rangeEnd - rangeStep) && next_.last();
+  }
+
+private:
+  ConstFlattenImpl<dimIndex + 1, ranges...> next_{};
+  typename BitsToRepresent<rangeBegin, rangeEnd>::type i_{rangeBegin};
+};
+
+}
+
+template <int... ranges>
+class ConstFlatten {
+
+public:
+  static constexpr int kDims = sizeof...(ranges) / 3;
+  static constexpr int kSize = ConstFlattenImpl<0, ranges...>::kSize;
+
+  int operator[](int i) {
+    #pragma HLS INLINE
+    return impl_[i];
+  }
+
+  template <int dim>
+  int get() {
+    #pragma HLS INLINE
+    static_assert(dim >= 0 && dim < kDims, "Invalid dimension specified.");
+    return impl_.template get<dim>();
+  }
+
+  void operator++() {
+    #pragma HLS INLINE
+    impl_.Increment();
+  }
+
+  void operator++(int) {
+    #pragma HLS INLINE
+    impl_.Increment();
+  }
+
+  constexpr int size() const {
+    return kSize;
+  }
+
+private:
+  ConstFlattenImpl<0, ranges...> impl_{};
+};
 
 }  // End namespace hlslib
--- test/TestFlatten.cpp
+++ test/TestFlatten.cpp
@@ -6,16 +6,56 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+TEST_CASE("Flatten") {
 
-TEST_CASE("Flatten", "[Flatten]") {
+  SECTION("Two loops") {
 
-  SECTION("Iteration space") {
-
-    auto nest = hlslib::Flatten(0, 10, 1, 0, 20, 2); 
+    auto nest = hlslib::Flatten(0, 10, 1, 0, 20, 2);
 
     for (size_t i = 0; i < nest.size(); ++i, ++nest) {
       REQUIRE(nest[0] == i / (20 / 2));
       REQUIRE(nest[1] == (2 * i) % 20);
+    }
+    
+  }
+  
+  SECTION("Negative bounds") {
+  
+    auto nest = hlslib::Flatten(-1, 1, 1, -4, -2, 1, -10, 0, 2);
+
+    for (int i = 0; i < nest.size(); ++i, ++nest) {
+      REQUIRE(nest[0] == -1 + i / 10);
+      REQUIRE(nest[1] == -4 + (i / 5) % 2);
+      REQUIRE(nest[2] == -10 + 2 * (i % 5));
+    }
+
+  }
+
+}
+
+TEST_CASE("ConstFlatten") {
+
+  SECTION("Two loops") {
+  
+    auto nest = hlslib::ConstFlatten<0, 10, 1, 0, 20, 2>();
+
+    for (size_t i = 0; i < nest.size(); ++i, ++nest) {
+      REQUIRE(nest[0] == i / (20 / 2));
+      REQUIRE(nest[1] == (2 * i) % 20);
+      REQUIRE(nest.template get<0>() == i / (20 / 2));
+      REQUIRE(nest.template get<1>() == (2 * i) % 20);
+    }
+
+  }
+  
+  SECTION("Negative bounds") {
+  
+    auto nest = hlslib::ConstFlatten<-1, 1, 1, -4, -2, 1, -10, 0, 2>();
+
+    for (int i = 0; i < nest.size(); ++i, ++nest) {
+      REQUIRE(nest[0] == -1 + i / 10);
+      REQUIRE(nest[1] == -4 + (i / 5) % 2);
+      REQUIRE(nest[2] == -10 + 2 * (i % 5));
     }
 
   }
