a7e0824784793be582725c1a346d5acbc72b2315
Johannes de Fine Licht <johannes@musicmedia.dk>
2018-09-18 18:55:11 +0200

    For the VCU1525 with SDAccel 2018.2, specifying memory banks without any binary loaded causes a segfault

--- test/TestSDAccelHostCode.cpp
+++ test/TestSDAccelHostCode.cpp
@@ -1,37 +1,34 @@
 /// @author    Johannes de Fine Licht (definelicht@inf.ethz.ch)
 /// @date      April 2017
-/// @copyright This software is copyrighted under the BSD 3-Clause License. 
+/// @copyright This software is copyrighted under the BSD 3-Clause License.
 
-#include "hlslib/SDAccel.h"
 #include <iostream>
+#include "aligned_allocator.h"
+#include "hlslib/SDAccel.h"
 
-using Data_t = int;
-constexpr int kMemSize = 1<<20;
+using Data_t = unsigned long;
+constexpr int kMemSize = 1 << 20;
 
 int main() {
-
   try {
-
     std::cout << "Creating context..." << std::endl;
     hlslib::ocl::Context context;
     std::cout << "Context created successfully." << std::endl;
 
     std::cout << "Initializing host memory..." << std::flush;
-    std::vector<Data_t> mem0Host(kMemSize, 5);
-    std::vector<Data_t> mem1Host(kMemSize, 0);
+    std::vector<Data_t, aligned_allocator<Data_t, 4096>> mem0Host(kMemSize, 5);
+    std::vector<Data_t, aligned_allocator<Data_t, 4096>> mem1Host(kMemSize, 0);
     std::cout << " Done." << std::endl;
 
     std::cout << "Creating device input buffer and copying from host..."
               << std::flush;
     auto mem0Device = context.MakeBuffer<Data_t, hlslib::ocl::Access::read>(
-        hlslib::ocl::MemoryBank::bank0, mem0Host.cbegin(), mem0Host.cend());
+        mem0Host.cbegin(), mem0Host.cend());
     std::cout << " Done." << std::endl;
 
     std::cout << "Creating device output buffer..." << std::flush;
-    // auto mem1Device = context.MakeBuffer<int, hlslib::ocl::Access::write>(
-    //     hlslib::ocl::MemoryBank::bank1, kMemSize);
-    auto mem1Device = context.MakeBuffer<int, hlslib::ocl::Access::write>(
-        hlslib::ocl::MemoryBank::bank1, mem1Host.cbegin(), mem1Host.cend());
+    auto mem1Device =
+        context.MakeBuffer<Data_t, hlslib::ocl::Access::write>(kMemSize);
     std::cout << " Done." << std::endl;
 
     std::cout << "Copying from input to output buffer..." << std::flush;
@@ -55,13 +52,11 @@ int main() {
     std::cerr << "Configuration failed with error: " << err.what() << std::endl;
     return 1;
   } catch (hlslib::ocl::RuntimeError const &err) {
-    std::cerr << "Runtime failed with error: " << err.what()
-              << std::endl;
+    std::cerr << "Runtime failed with error: " << err.what() << std::endl;
     return 2;
   }
 
   std::cout << "SDAccel platform successfully verified." << std::endl;
 
   return 0;
-
 }
