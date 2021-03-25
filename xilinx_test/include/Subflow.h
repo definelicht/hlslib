04bd33e98613c6327338132676e8fa363e83de3b
Johannes de Fine Licht <johannes@musicmedia.dk>
2021-03-25 15:33:19 +0100

    clang-format and adapt naming conventions

--- xilinx_test/include/Subflow.h
+++ xilinx_test/include/Subflow.h
@@ -1,7 +1,6 @@
 #pragma once
 
-
 using Data_t = int;
-constexpr int dataSize = 128;
+constexpr int kSize = 128;
 
 extern "C" void Subflow(const Data_t* memIn, Data_t* memOut);
--- xilinx_test/kernels/Subflow.cpp
+++ xilinx_test/kernels/Subflow.cpp
@@ -3,59 +3,49 @@
 #include "hlslib/xilinx/Simulation.h"
 #include "hlslib/xilinx/Stream.h"
 
-
-void readIn(const Data_t* memIn, hlslib::Stream<Data_t>& inPipe) {
-  for (unsigned i=0; i<dataSize; ++i) {
-    #pragma HLS pipeline II=1
-    #pragma HLS flatten
+void ReadIn(const Data_t* memIn, hlslib::Stream<Data_t>& inPipe) {
+  for (unsigned i = 0; i < kSize; ++i) {
+    #pragma HLS PIPELINE II=1
     inPipe.Push(memIn[i]);
   }
 }
 
-void add_one(
-  hlslib::Stream<Data_t>& inPipe, hlslib::Stream<Data_t>& internal
-) {
-  for (unsigned i=0; i<dataSize; ++i) {
-    #pragma HLS pipeline II=1
-    #pragma HLS flatten
+void AddOne(hlslib::Stream<Data_t>& inPipe, hlslib::Stream<Data_t>& internal) {
+  for (unsigned i = 0; i < kSize; ++i) {
+    #pragma HLS PIPELINE II=1
     internal.Push(inPipe.Pop() + 1);
   }
 }
 
-void multiply_by_two(
-  hlslib::Stream<Data_t>& internal, hlslib::Stream<Data_t>& outPipe
-) {
-  for (unsigned i=0; i<dataSize; ++i) {
-    #pragma HLS pipeline II=1
-    #pragma HLS flatten
+void MultiplyByTwo(hlslib::Stream<Data_t>& internal,
+                   hlslib::Stream<Data_t>& outPipe) {
+  for (unsigned i = 0; i < kSize; ++i) {
+    #pragma HLS PIPELINE II=1
     outPipe.Push(internal.Pop() * 2);
   }
 }
 
-void subtask(
-  hlslib::Stream<Data_t>& inPipe, hlslib::Stream<Data_t>& outPipe
-) {
+void Subtask(hlslib::Stream<Data_t>& inPipe, hlslib::Stream<Data_t>& outPipe) {
   HLSLIB_SUBFLOW_INIT();
   hlslib::Stream<Data_t> internal;
-  HLSLIB_DATAFLOW_FUNCTION(add_one, inPipe, internal);
-  HLSLIB_DATAFLOW_FUNCTION(multiply_by_two, internal, outPipe);
+  HLSLIB_DATAFLOW_FUNCTION(AddOne, inPipe, internal);
+  HLSLIB_DATAFLOW_FUNCTION(MultiplyByTwo, internal, outPipe);
   HLSLIB_SUBFLOW_FINALIZE();
 }
 
-void writeOut(hlslib::Stream<Data_t>& outPipe, Data_t* memOut) {
-  for (unsigned i=0; i<dataSize; ++i) {
-    #pragma HLS pipeline II=1
-    #pragma HLS flatten
+void WriteOut(hlslib::Stream<Data_t>& outPipe, Data_t* memOut) {
+  for (unsigned i = 0; i < kSize; ++i) {
+    #pragma HLS PIPELINE II=1
     memOut[i] = outPipe.Pop();
- }
+  }
 }
 
-extern "C" void Subflow(const Data_t* memIn, Data_t* memOut) {
-  #pragma HLS dataflow
+void Subflow(const Data_t* memIn, Data_t* memOut) {
+  #pragma HLS DATAFLOW
   HLSLIB_DATAFLOW_INIT();
   hlslib::Stream<Data_t> inPipe, outPipe;
-  HLSLIB_DATAFLOW_FUNCTION(readIn, memIn, inPipe);
-  HLSLIB_ADD_SUBFLOW(subtask, inPipe, outPipe);
-  HLSLIB_DATAFLOW_FUNCTION(writeOut, outPipe, memOut);
+  HLSLIB_DATAFLOW_FUNCTION(ReadIn, memIn, inPipe);
+  HLSLIB_ADD_SUBFLOW(Subtask, inPipe, outPipe);
+  HLSLIB_DATAFLOW_FUNCTION(WriteOut, outPipe, memOut);
   HLSLIB_DATAFLOW_FINALIZE();
 }
