026e6b40c7290ddac491a1fa769799bd075e9692
jnice-81 <68808325+jnice-81@users.noreply.github.com>
2021-05-26 15:48:22 +0200

    Update xilinx_test/kernels/HBMandBlockCopy.cpp
    Co-authored-by: definelicht <johannes@musicmedia.dk>

--- xilinx_test/kernels/HBMandBlockCopy.cpp
+++ xilinx_test/kernels/HBMandBlockCopy.cpp
@@ -3,7 +3,7 @@ constexpr int kDataSize = 1024;
 extern "C" {
 void HBMandBlockCopy(int *hbm0, int *ddr1, int *hbm13, int *hbm20, int *hbm31,
                      int *ddr0) {
-  for (int i = 0; i < DATA_SIZE; i++) {
+  for (int i = 0; i < kDataSize; i++) {
     #pragma HLS PIPELINE II=1
     ddr0[i] = hbm0[i] + ddr1[i] + hbm13[i] + hbm20[i] + hbm31[i];
   }
