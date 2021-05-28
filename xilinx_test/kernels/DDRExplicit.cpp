501ee56d33ec13a0c5d8c5851245335b0153918b
jnice-81 <68808325+jnice-81@users.noreply.github.com>
2021-05-28 17:54:16 +0200

    Update xilinx_test/kernels/DDRExplicit.cpp
    Co-authored-by: definelicht <johannes@musicmedia.dk>

--- xilinx_test/kernels/DDRExplicit.cpp
+++ xilinx_test/kernels/DDRExplicit.cpp
@@ -6,7 +6,7 @@
 extern "C" {
 void DDRExplicit(int *ddr0, int *ddr1) {
   for (int i = 0; i < DATA_SIZE; i++) {
-#pragma HLS PIPELINE II = 1
+    #pragma HLS PIPELINE II=1
     ddr0[i] = ddr1[i];
   }
 }
