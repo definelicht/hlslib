cfb2f243ec615f152897e2ed83e0616078d12bec
Jannis Widmer <widmerja@student.ethz.ch>
2021-05-28 17:58:04 +0200

    Changed to constexpr

--- xilinx_test/kernels/DDRExplicit.cpp
+++ xilinx_test/kernels/DDRExplicit.cpp
@@ -1,11 +1,11 @@
 /// @author    Jannis Widmer (widmerja@ethz.ch)
 /// @copyright This software is copyrighted under the BSD 3-Clause License.
 
-#define DATA_SIZE 1024
+constexpr int kDataSize = 1024;
 
 extern "C" {
 void DDRExplicit(int *ddr0, int *ddr1) {
-  for (int i = 0; i < DATA_SIZE; i++) {
+  for (int i = 0; i < kDataSize; i++) {
     #pragma HLS PIPELINE II=1
     ddr0[i] = ddr1[i];
   }
