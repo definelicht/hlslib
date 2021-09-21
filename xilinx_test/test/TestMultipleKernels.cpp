dde7ee14281cce8f26cdbbd0ea0fe87ae963bd4c
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2021-09-21 15:42:48 +0200

    Run hardware emulation test, allow stream placeholders

--- xilinx_test/test/TestMultipleKernels.cpp
+++ xilinx_test/test/TestMultipleKernels.cpp
@@ -1,8 +1,11 @@
+#include <cstdlib>
+
 #include "MultipleKernels.h"
 #include "catch.hpp"
 #include "hlslib/xilinx/OpenCL.h"
 
 TEST_CASE("MultipleKernels") {
+  setenv("XCL_EMULATION_MODE", "hw_emu", true);
   using namespace hlslib::ocl;
   Context context;
   auto program = context.MakeProgram("MultipleKernels_hw_emu.xclbin");
@@ -10,8 +13,8 @@ TEST_CASE("MultipleKernels") {
   auto memory = context.MakeBuffer<uint64_t, Access::readWrite>(
       memory_host.cbegin(), memory_host.cend());
   hlslib::Stream<uint64_t> pipe;
-  auto first_kernel = program.MakeKernel("FirstKernel");
-  auto second_kernel = program.MakeKernel("SecondKernel");
+  auto first_kernel = program.MakeKernel("FirstKernel", memory, Stream, 128);
+  auto second_kernel = program.MakeKernel("SecondKernel", Stream, memory, 128);
   auto first_future = first_kernel.ExecuteTaskAsync();
   auto second_future = second_kernel.ExecuteTaskAsync();
   first_future.wait();
