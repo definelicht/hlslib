16afd677a1409ace152cb5aa29acbf1a38cca350
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2021-11-29 16:58:07 +0100

    Reduce test size

--- xilinx_test/test/TestMultipleKernels.cpp
+++ xilinx_test/test/TestMultipleKernels.cpp
@@ -4,6 +4,8 @@
 #include "catch.hpp"
 #include "hlslib/xilinx/OpenCL.h"
 
+constexpr int kTestSize = 16;
+
 #ifndef HLSLIB_SIMULATE_OPENCL
 TEST_CASE("MultipleKernels Hardware Emulation") {
   setenv("XCL_EMULATION_MODE", "hw_emu", true);
@@ -14,22 +16,22 @@ TEST_CASE("MultipleKernels Simulation") {
   hlslib::ocl::Context context;
   auto program = context.MakeProgram("");
 #endif
-  std::vector<uint64_t> memory_host(128, 1);
+  std::vector<uint64_t> memory_host(kTestSize, 1);
   auto memory = context.MakeBuffer<uint64_t, hlslib::ocl::Access::readWrite>(
       memory_host.cbegin(), memory_host.cend());
   hlslib::Stream<uint64_t> pipe;
   auto first_kernel =
       program.MakeKernel(FirstKernel, "FirstKernel", memory,
-                         hlslib::ocl::SimulationOnly(pipe), 128);
+                         hlslib::ocl::SimulationOnly(pipe), kTestSize);
   auto second_kernel =
       program.MakeKernel(SecondKernel, "SecondKernel",
-                         hlslib::ocl::SimulationOnly(pipe), memory, 128);
+                         hlslib::ocl::SimulationOnly(pipe), memory, kTestSize);
   auto first_future = first_kernel.ExecuteTaskAsync();
   auto second_future = second_kernel.ExecuteTaskAsync();
   first_future.wait();
   second_future.wait();
   memory.CopyToHost(memory_host.begin());
-  for (int i = 0; i < 128; ++i) {
+  for (int i = 0; i < kTestSize; ++i) {
     REQUIRE(memory_host[i] == 4);
   }
 }
