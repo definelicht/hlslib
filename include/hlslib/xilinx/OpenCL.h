e8388b9fdb915084a6c61789e0372a5c67bb6155
Johannes de Fine Licht <johannes@musicmedia.dk>
2024-03-26 19:20:29 +0100

    Use new OpenCL header convention.

--- include/hlslib/xilinx/OpenCL.h
+++ include/hlslib/xilinx/OpenCL.h
@@ -16,7 +16,7 @@
 #define CL_HPP_TARGET_OPENCL_VERSION 120
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#include <CL/cl2.hpp>
+#include <CL/opencl.hpp>
 
 namespace hlslib {
 
